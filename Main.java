5db559a1d41aaf5bf31df822ab4eafd15f6f002b
Arnold Smithson <arnoldsmithson@outlook.com>
2020-05-29 11:32:48 -0600

    update for initial release

--- Main.java
+++ Main.java
@@ -1,12 +1,11 @@
 package slide;
 
 import javax.swing.*;
-import java.awt.*;
-import java.io.IOException;
 
-public class Main extends JFrame {
 
-    public static void main(String[] args) throws IOException, FontFormatException {
+public class Main {
+
+    public static void main(String[] args) {
         ProgramManager start = new ProgramManager(0);//USE 0 FOR MAIN MONITOR, 1 FOR SECOND MONITOR
         start.setVisible(true);
     }
